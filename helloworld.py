d64e3bd10fe089675835c9fc30d9600eb7b074c8
Jorj Xenos <jxenos@earthcam.com>
2018-09-06 09:19:59 -0400

    update to use py2 input_raw rather than py3 input

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,11 @@
 print("Hello World")
-myName = input ("What is your name? ")
+myName = raw_input ("What is your name? ")
+myVar = raw_input("What is the coolest number? ")
 print(myName)
+
+if(myName == 1234 and myVar == 0):
+	print("your name is a number....")
+elif(myName == 'asdf'):
+	print("your name is trash")
+else:
+	print("name")
