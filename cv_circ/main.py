b1d39449020e04201702d16db1b85c16cd2f3ca9
Jorj Xenos <jxenos@earthcam.com>
2018-09-07 11:28:13 -0400

    adjust the script for testing

--- cv_circ/main.py
+++ cv_circ/main.py
@@ -3,21 +3,35 @@ import glob
 import numpy as np
 
 def main():
-	img_arr = glob.glob("./imgs/*.bmp")
-	imgpath = "./imgs/correct.bmp"
+	#img_arr = glob.glob("./imgs/*.bmp")
+	imgpath = "./imgs/13525_sunset_3.bmp"
+	#imgpath = "./imgs/detect_circles_simple.jpg"
 
+	n1 = raw_input('minVal\n')
+	n2 = raw_input('maxVal\n')
+
+	i = 0
+
+	#for imgpath in img_arr:
 	img = cv2.imread(imgpath, 1)
-	edges = cv2.Canny(img, 15, 90)
-	circ = cv2.HoughCircles(edges, cv2.cv.CV_HOUGH_GRADIENT, 1.2, 100)
+	#img = cv2.GaussianBlur(img, (5,5), 0)
+	#img = cv2.Canny(img, float(n1), float(n2))
+	#img = cv2.Canny(img, 10, 75)
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	
+	circ = cv2.HoughCircles(img, cv2.cv.CV_HOUGH_GRADIENT, 1, 80, param1=float(n1), param2=float(n2),minRadius=0,maxRadius=0)
 
+	img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 	if circ is not None:
 		circ = np.round(circ[0, :]).astype("int")
 
 		for (x, y, r) in circ:
 			cv2.circle(img, (x,y), r, (0, 255, 0), 4)
-			
-	
-	out = "./output/"
-	cv2.imwrite(out + "test.bmp", edges)
+
+	else:
+		cv2.circle(img, (20,20), 10, (0,0,255), -1)
+
+	out= "./output/"
+	cv2.imwrite(out+"batch_"+n1+"_"+n2+"_"+str(i)+".bmp", img)
 
 main()
--- cv_circ/test.py
+++ cv_circ/test.py
@@ -7,8 +7,8 @@ def main():
 	imgpath = "./imgs/test.bmp"
 
 	img = cv2.imread(imgpath)
-	edges = img
-	#edges = cv2.GaussianBlur(img, (11,11),0)
+	#edges = img
+	edges = cv2.GaussianBlur(img, (11,11),0)
 	edges = cv2.Canny(edges, 15, 90)
 	edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
 	cv2.circle(edges, (500,500), 30, (255, 255, 0), 4)
