7714d70329308d31091eaa2700abbf630803a71f
Jorj Xenos <jxenos@earthcam.com>
2018-09-13 11:12:15 -0400

    keep updating batch process for testing

--- cv_circ/main.py
+++ cv_circ/main.py
@@ -17,31 +17,33 @@ def main():
 	while (i < len(a)-1):
 		x = 0
 		for imgpath in img_arr:
-			img = cv2.imread(imgpath, 1)
-			img = cv2.GaussianBlur(img, (5,5), 0)
-			#img = cv2.Canny(img, float(n1), float(n2))
-			#img = cv2.Canny(img, 10, 75)
+			img_orig = cv2.imread(imgpath, 1)
+			img = cv2.GaussianBlur(img_orig, (5,5), 0)
 			img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+			#img = cv2.Canny(img, float(n1), float(n2))
+			img = cv2.Canny(img, 10, 75)
+
 
 			print(i)
 			n1 = a[i][0]
 			n2 = a[i][1]
 
 			#circ = cv2.HoughCircles(img, cv2.cv.CV_HOUGH_GRADIENT, 1.5, 200, param1=float(n1), param2=float(n2),minRadius=0,maxRadius=300)
-			circ = cv2.HoughCircles(img, cv2.cv.CV_HOUGH_GRADIENT, a[i][0], a[i][1], param1=7, param2=50,minRadius=3,maxRadius=150)
-
+			#circ = cv2.HoughCircles(img, cv2.cv.CV_HOUGH_GRADIENT, a[i][0], a[i][1], param1=7, param2=50,minRadius=3,maxRadius=150)
+			circ = cv2.HoughCircles(img, cv2.cv.CV_HOUGH_GRADIENT, a[i][0], a[i][1], param1=2, param2=150,minRadius=3,maxRadius=150)
 			img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 			if circ is not None:
 				circ = np.round(circ[0, :]).astype("int")
 
 				for (x, y, r) in circ:
-					cv2.circle(img, (x,y), r, (0, 255, 0), 4)
+					cv2.circle(img_orig, (x,y), r, (0, 255, 0), 4)
 
 			else:
-				cv2.circle(img, (20,20), 10, (0,0,255), -1)
+				cv2.circle(img_orig, (20,20), 10, (0,0,255), -1)
 
 			out= "./output/"
-			cv2.imwrite(out+"batch_"+str(n1)+"_"+str(n2)+"_"+str(i)+str(x)+".bmp", img)
+			cv2.imwrite(out+"batch_"+str(n1)+"_"+str(n2)+"_"+str(i)+str(x)+".bmp", img_orig)
+			cv2.imwrite(out+"batch_"+str(n1)+"_"+str(n2)+"_"+str(i)+str(x)+"_canny.bmp", img)
 			x += 1
 		i += 1
 
